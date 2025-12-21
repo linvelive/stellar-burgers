36177f13047c8755d5d4ce30d81fdd41206ca3ac
linvelive <alina10.1179@gmail.com>
2025-12-21 15:07:31 -0500

    fix: integrate Redux for fetching and displaying user orders in ProfileOrders component

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,21 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import {
+  fetchOrders,
+  getOrdersSelector
+} from '../../services/slices/ordersSlice';
+import { useDispatch, useSelector } from '../../services/store';
 
 export const ProfileOrders: FC = () => {
+  const dispatch = useDispatch();
+
   /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const orders: TOrder[] = useSelector(getOrdersSelector);
+
+  useEffect(() => {
+    dispatch(fetchOrders());
+  }, [dispatch]);
 
   return <ProfileOrdersUI orders={orders} />;
 };
