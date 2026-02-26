941ef4c52c082d682c73728b6a5a37a2c205f38c
linvelive <alina10.1179@gmail.com>
2026-02-26 10:49:32 -0500

    fix: ensure index.css is imported in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
 import store from './services/store';
+import './index.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
